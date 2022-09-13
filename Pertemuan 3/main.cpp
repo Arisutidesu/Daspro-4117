d93ca0661565c07457454bc56aee16aa6454f11d
Arisutidesu <112882463+Arisutidesu@users.noreply.github.com>
2022-09-13 11:43:10 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 2022;
+    cout << "Anime : Hello World" << x;
+
+    int w = 2000;
+    float p = 193.5;
+    string k = "Takanashi Kiara";
+    char h = 'H';
+
+    cout << "\nWatashi no okane : " << w << endl;
+    cout << "Peeji : " << p;
+    cout << "\nVtuber : " << k;
+    cout << "\nSize : " << h;
+}
